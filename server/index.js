5a32bda46e87f51d1c8d55c74f5201b52cc9606e
McCarroll, Alexandra <Alexandra.McCarroll@capco.com>
2019-06-18 11:53:32 +0100

    trying to resolve issues

--- server/index.js
+++ server/index.js
@@ -20,9 +20,14 @@ setup(app, {
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
-// const customHost = argv.host || process.env.HOST;
-// const host = customHost || null; // Let http.Server use its default IPv6/4 host
-// const prettyHost = customHost || 'localhost';
+const customHost = argv.host || process.env.HOST;
+const host = customHost || null; // Let http.Server use its default IPv6/4 host
+const prettyHost = customHost || 'localhost';
 
 // Start your app.
-app.listen(process.env.PORT || 3000);
+app.listen(port, host, (err) => {
+  if (err) {
+    return logger.error(err.message);
+  }
+  logger.appStarted(port, prettyHost);
+});
