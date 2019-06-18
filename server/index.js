473600f468eabfdcf6a1b4f8d94fe5ebd68462f9
McCarroll, Alexandra <Alexandra.McCarroll@capco.com>
2019-06-18 11:39:37 +0100

    fixing port listen

--- server/index.js
+++ server/index.js
@@ -20,14 +20,9 @@ setup(app, {
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
-const customHost = argv.host || process.env.HOST;
-const host = customHost || null; // Let http.Server use its default IPv6/4 host
-const prettyHost = customHost || 'localhost';
+// const customHost = argv.host || process.env.HOST;
+// const host = customHost || null; // Let http.Server use its default IPv6/4 host
+// const prettyHost = customHost || 'localhost';
 
 // Start your app.
-app.listen(port, host, (err) => {
-  if (err) {
-    return logger.error(err.message);
-  }
-  logger.appStarted(port, prettyHost);
-});
+app.listen(process.env.PORT || 3000);
